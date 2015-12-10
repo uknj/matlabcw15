ec09956a541d52e90d53ad71088bee5796ddd8a1
uk00015 <uk00015@ETIB20.surrey.ac.uk>
2015-12-10 17:21:49 +0000

    applyhole m file

--- grid_points.m
+++ grid_points.m
@@ -1,5 +1,5 @@
 function [dx, dy, n] = grid_points(x_width, y_width, ni, nj) 
-%Gridpoints: This function gives step sizes for specified conditions.
+% Gridpoints: This function gives step sizes for specified conditions.
 %   This function takes values for the dimensions of the square and the
 %   number of the required points along the x and y axis. It returns dx and
 %   dy the step sizes. It also returns n, the total number of grid points.
