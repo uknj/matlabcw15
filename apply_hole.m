b26641476d2772af1928b5f5d647f076b4db841d
uk00015 <uk00015@ETIB20.surrey.ac.uk>
2015-12-10 17:38:15 +0000

    changing apply hole to ensure row is set to 0 first then change  the individual point

--- apply_hole.m
+++ apply_hole.m
@@ -26,8 +26,8 @@ for i = 1:ni
        % if (x nodes >= x hole and <= x hole + dim) and (y nodes >= y
        % hole and <= y hole + dim)
        if (x_hole <= i * dx <= x_hole + dim_h) && (y_hole <= j * dy <= y_hole + dim_h)
-           M(C, C) = 1; % Set the matrix at C,C as one, and the rest of the row as 0.
            M(C, :) = 0;
+           M(C, C) = 1; % Set the matrix at C,C as one, and the rest of the row as 0.
            c(C) = T_hole; % Set the b matrix to the temperature of the hole.
        end
     end
